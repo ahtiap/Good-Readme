79078bbb3c7cc893a847749ddf07bfe06daae152
unknown <labsar.ahmed@outlook.fr>
2020-07-30 18:39:37 -0400

    called the writeToFile function with the file name and the markdown generator function

--- Develop/index.js
+++ Develop/index.js
@@ -1,49 +1,51 @@
-var inquirer= require("inquirer");
-var fs= require("fs");
-var generateMarkDown= require("./utils/generateMarkdown");
-var path= require("path");
+var inquirer = require("inquirer");
+var fs = require("fs");
+var generateMarkDown = require("./utils/generateMarkdown");
+var path = require("path");
 // array of questions for user
 const questions = [
-{
+  {
     type: "input",
     name: "title",
-    message: "What is the Title of your project"
-},
-{
+    message: "What is the Title of your project",
+  },
+  {
     type: "input",
     name: "description",
-    message: "Describe your Project."
-},
-{
+    message: "Describe your Project.",
+  },
+  {
     type: "list",
     name: "license",
     message: "Choose a License.",
-    choices:["Apache License 2.0","MIT License","Eclipse Public License2.0"]
-},
-{
+    choices: ["Apache License 2.0", "MIT License", "Eclipse Public License2.0"],
+  },
+  {
     type: "input",
     name: "username",
-    message: "What is your Github Username?"
-},
-{
+    message: "What is your Github Username?",
+  },
+  {
     type: "input",
     name: "email",
-    message: "What is your Email address?"
-}
+    message: "What is your Email address?",
+  },
 ];
 // file name
-var readme="readme.md";
+var readme = "readme.md";
 // function to write README file
 function writeToFile(fileName, data) {
-return fs.writeFileSync(path.join(process.cwd(), fileName),data);
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // function to initialize program
 function init() {
-inquirer.prompt(questions).then(res =>{
+  // ask the user questions and get the answers
+  inquirer.prompt(questions).then((res) => {
     console.log(res);
-    writeToFile(readme,generateMarkDown(res));
-});
+    //create the file using the file name and markdown on the response
+    writeToFile(readme, generateMarkDown(res));
+  });
 }
 
 // function call to initialize program
