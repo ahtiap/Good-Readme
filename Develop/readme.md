fbbd56fa6b022867fefb2186ad10e194698ffe42
unknown <labsar.ahmed@outlook.fr>
2020-07-30 19:00:04 -0400

    added main section titles with markdown

--- Develop/readme.md
+++ Develop/readme.md
@@ -1,4 +1,11 @@
-# **readme**
+# **ReadMe**
   ## **_Description_**:
-  
+  This project renders a readme with markdown using the users entries.
+  ## **_Table of Contents_**:
+  ### **_Installation_**:
+  ### **_Usage_**:
+  ### **_License_**:
+  ### **_Contributing_**:
+  ### **_Tests_**:
+  ### **_Questions_**:
   
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -3,7 +3,14 @@ function generateMarkdown(data) {
   return `# **${data.title}**
   ## **_Description_**:
   ${data.description}
+  ## **_Table of Contents_**:
+  ### **_Installation_**:
+  ### **_Usage_**:
+  ### **_License_**:
+  ### **_Contributing_**:
+  ### **_Tests_**:
+  ### **_Questions_**:
   `;
 }
-
+// Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 module.exports = generateMarkdown;
